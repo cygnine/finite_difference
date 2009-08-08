606bb53b64739431a33c868966248f43d833868c
Akil Narayan <anaray@dam.brown.edu>
2009-08-08 12:07:25 -0400

    Change to handles rubric

--- handles__.m
+++ handles__.m
@@ -1,7 +1,11 @@
-function[hs] = handles__()
-% [HS] = HANDLES__()
+function[hs,pathadditions] = handles__()
+% [HS,PATHADDITIONS] = HANDLES__()
 %
-%     Returns directory pointers for common module
+%     Returns directory pointers for common module in HS. PATHADDITIONS is a
+%     cell array with a string in each element indicated paths to add to the
+%     global path structure. 
 
 % This is by default
 hs.base = fileparts(mfilename('fullpath'));
+
+pathadditions = cell(0);
